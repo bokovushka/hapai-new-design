49c76a7367e414fd0666b2adb526c38a99f594cc
bokovushka <andriibokovyi@gmail.com>
2023-01-11 22:44:51 +0200

    11.01

---
+++ js/main.js
@@ -0,0 +1,160 @@
+// меню
+
+$(document).ready(function () {
+	let body_lock = document.querySelector('body');
+	let menuBtn = document.querySelector('.menu-btn');
+	let menu = document.querySelector('.menu');
+	let language = document.querySelector('.language');
+	let header_active = document.querySelector('.header-menu');
+
+	menuBtn.addEventListener('click', function () {
+		menuBtn.classList.toggle('active');
+		menu.classList.toggle('active');
+		language.classList.toggle('active');
+		header_active.classList.toggle('active');
+		body_lock.classList.toggle('lock-menu-m');
+	})
+});
+
+// height = max height
+
+$(document).ready(function () {
+	function setEqualHeight(columns) {
+		var tallestcolumn = 0;
+		columns.each(function () {
+			currentHeight = $(this).height();
+			if (currentHeight > tallestcolumn) {
+				tallestcolumn = currentHeight;
+			}
+		});
+		columns.height(tallestcolumn);
+	}
+
+	setEqualHeight($(".item-loop__txtblock .heading"));
+	setEqualHeight($(".article__swiper .swiper-slide"));
+	setEqualHeight($(".happy-customers__swiper .swiper-slide"));
+	setEqualHeight($(".favorite__main .item-loop__link"));
+	setEqualHeight($(".item-loop__txtblock"));
+});
+
+//favorite
+
+$(document).on('click', '.btn-favor', function () {
+	if ($(this).attr('data-status') === 'favorite') {
+		$(this).removeAttr('data-status', 'favorite')
+	} else {
+		$(this).attr('data-status', 'favorite')
+	}
+});
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+	var anchor = $(this);
+	$("html, body")
+		.stop()
+		.animate(
+			{
+				scrollTop: $(anchor.attr("href")).offset().top - 20,
+			},
+			800
+		);
+	e.preventDefault();
+});
+
+//popup-authorization
+
+$(function () {
+	$(".checkbox input").checkboxradio();
+});
+$("#popup-authorization .login .txt-link").click(function () {
+	$(".login").addClass("hide");
+	$(".register").removeClass("hide");
+
+});
+$("#popup-authorization .register .txt-link").click(function () {
+	$(".register").addClass("hide");
+	$(".login").removeClass("hide");
+});
+
+//faq-item
+$(function () {
+	$("#accordion").accordion({
+		collapsible: true,
+		active: false,
+		heightStyle: "content"
+	});
+});
+
+// MAIN SEARCH
+
+// $(function () {
+// 	$.widget("custom.catcomplete", $.ui.autocomplete, {
+// 		_create: function () {
+// 			this._super();
+// 			this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+// 		},
+// 		_renderMenu: function (ul, items) {
+// 			var that = this,
+// 				currentCategory = "";
+// 			$.each(items, function (index, item) {
+// 				var li;
+// 				if (item.category != currentCategory) {
+// 					ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
+// 					currentCategory = item.category;
+// 				}
+// 				li = that._renderItemData(ul, item);
+// 				if (item.category) {
+// 					li.attr("aria-label", item.category + " : " + item.label);
+// 				}
+// 			});
+// 		}
+// 	});
+// 	var data = [
+// 		{ label: "Audi A3", category: "" },
+// 		{ label: "Audi A4", category: "" },
+// 		{ label: "Audi A5", category: "" },
+// 		{ label: "Audi A7", category: "" },
+// 		{ label: "Продати Audi", category: "Послуги" },
+// 		{ label: "Купити Audi", category: "Послуги" },
+// 		{ label: "Audi захоплює дух часу цим автомобілем: перший Audi 80 був представлений 50 років тому", category: "Блог" },
+// 		{ label: "Audi додає Apple Music до широкого спектру своїх моделей", category: "Блог" }
+// 	];
+//
+// 	$("#search").catcomplete({
+// 		delay: 0,
+// 		source: data
+// 	});
+// });
+
+
+$(document).ready(function () {
+	$("#datepicker").datepicker({
+		// changeMonth: true,
+		// changeYear: true,
+		showOn: "button",
+		showOtherMonths: true,
+		selectOtherMonths: true
+	});
+
+	$(".date-link").click(function () {
+		$("#datepicker").datepicker("show");
+	});
+});
+
+//Back to Top Button
+$(document).ready(function () {
+	var btn = $("#button_top");
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass("show");
+		} else {
+			btn.removeClass("show");
+		}
+	});
+
+	btn.on("click", function (e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, "300");
+	});
+});
+
---
+++ js/slider.js
@@ -0,0 +1,235 @@
+// карусель рекомендуемое
+
+new Swiper(".partners__swiper", {
+	spaceBetween: 8,
+	grabCursor: true,
+	breakpoints: {
+		1440: {
+			slidesPerView: 4,
+		},
+		1280: {
+			slidesPerView: 3,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		640: {
+			slidesPerView: 1.7,
+		},
+		360: {
+			slidesPerView: 1.1,
+		}
+	},
+	navigation: {
+		prevEl: "#qa-loop .swiper__buttons .icon-u_arrow-left",
+		nextEl: "#qa-loop .swiper__buttons .icon-u_arrow-right"
+	},
+})
+
+// карусель на карточках товара
+
+function swiperItemGallery() {
+	new Swiper('.swiper-item-gallery', {
+		direction: 'horizontal',
+		nested: true,
+		grabCursor: true,
+		navigation: {
+			nextEl: '.item-gallery-button .btn-item-swiper.icon-right',
+			prevEl: '.item-gallery-button .btn-item-swiper.icon-left',
+		},
+	});
+}
+swiperItemGallery();
+
+// slider gallery
+
+if (window.innerWidth < 993) {
+	var swipercontainer = $('.ic-gallery');
+	var swiperwrapper = $('.ic-img-loop');
+	var swiperslide = $('.ic-img');
+	$('.ic-gallery').addClass('swiper-container');
+	$('.ic-img-loop').addClass('swiper-wrapper');
+	$('.ic-img').addClass('swiper-slide');
+	var mySwiper2 = new Swiper('.ic-gallery', {
+		direction: 'horizontal',
+		slidesPerView: 1,
+		spaceBetween: 20,
+		navigation: {
+			prevEl: ".ic-gallery .item-gallery-button .btn__swiper-prev",
+			nextEl: ".ic-gallery .item-gallery-button .btn__swiper-next"
+		},
+		breakpoints: {
+			640: {
+				slidesPerView: 2,
+			}
+		},
+	});
+}
+
+// article Swiper slider
+
+var articleSwiper = new Swiper('.article__swiper', {
+	direction: 'horizontal',
+	grabCursor: true,
+	spaceBetween: 20,
+	freeMode: true,
+	breakpoints: {
+		1280: {
+			slidesPerView: 4,
+		},
+		992: {
+			slidesPerView: 3.2,
+		},
+		768: {
+			slidesPerView: 2.4,
+		},
+		640: {
+			slidesPerView: 2.1,
+			spaceBetween: 8,
+		},
+		480: {
+			slidesPerView: 1.6,
+			spaceBetween: 8,
+		},
+		360: {
+			slidesPerView: 1.1,
+			spaceBetween: 8,
+		}
+	},
+	navigation: {
+		prevEl: ".team .swiper__buttons .icon-u_arrow-left",
+		nextEl: ".team .swiper__buttons .icon-u_arrow-right"
+	},
+});
+
+// team Swiper slider
+
+var teamSwiper = new Swiper('.team__swiper', {
+	direction: 'horizontal',
+	grabCursor: true,
+	spaceBetween: 20,
+	freeMode: true,
+	breakpoints: {
+		1280: {
+			slidesPerView: 4,
+		},
+		992: {
+			slidesPerView: 3.2,
+		},
+		768: {
+			slidesPerView: 2.4,
+		},
+		640: {
+			slidesPerView: 2.1,
+		},
+		480: {
+			slidesPerView: 1.6,
+		},
+		360: {
+			slidesPerView: 1.2,
+		}
+	},
+});
+
+//car-style
+
+var articleSwiper = new Swiper('.car-style__swiper', {
+	direction: 'horizontal',
+	grabCursor: true,
+	spaceBetween: 12,
+	freeMode: true,
+	breakpoints: {
+		1280: {
+			slidesPerView: 5,
+		},
+		1024: {
+			slidesPerView: 4.1,
+		},
+		992: {
+			slidesPerView: 3.9,
+		},
+		768: {
+			slidesPerView: 2.9,
+		},
+		640: {
+			slidesPerView: 2.2,
+		},
+		480: {
+			slidesPerView: 1.8,
+		},
+		360: {
+			slidesPerView: 1.3,
+		}
+	},
+});
+
+//company-partners
+new Swiper(".company-partners__swiper", {
+	spaceBetween: 20,
+	grabCursor: true,
+	autoHeight: false,
+	loop: true,
+	breakpoints: {
+		1440: {
+			slidesPerView: 5,
+		},
+		1280: {
+			slidesPerView: 4,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		360: {
+			slidesPerView: 1,
+			grid: {
+				rows: 2,
+			},
+			loop: false,
+		}
+	},
+	navigation: {
+		prevEl: ".company-partners .swiper__buttons .icon-u_arrow-left",
+		nextEl: ".company-partners .swiper__buttons .icon-u_arrow-right"
+	},
+})
+
+
+//loan-leasing
+new Swiper(".loan-leasing .leasing__swiper", {
+	spaceBetween: 20,
+	grabCursor: true,
+	loop: true,
+	breakpoints: {
+		992: {
+			slidesPerView: 3,
+		},
+		640: {
+			slidesPerView: 2,
+		}
+	},
+	navigation: {
+		prevEl: ".loan-leasing .leasing-swiper__buttons .icon-u_arrow-left",
+		nextEl: ".loan-leasing .leasing-swiper__buttons .icon-u_arrow-right"
+	},
+})
+
+new Swiper(".loan-leasing .loan__swiper", {
+	spaceBetween: 20,
+	grabCursor: true,
+	loop: true,
+	breakpoints: {
+		992: {
+			slidesPerView: 3,
+		},
+		640: {
+			slidesPerView: 2,
+		}
+	},
+	navigation: {
+		prevEl: ".loan-leasing .loan-swiper__buttons .icon-u_arrow-left",
+		nextEl: ".loan-leasing .loan-swiper__buttons .icon-u_arrow-right"
+	},
+})
